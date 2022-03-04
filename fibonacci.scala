9128a05d740fc43d5c5a56ba7540e1506023e55b
ndrohith09 <ndrohith09@gmail.com>
2022-03-04 09:48:04 +0530

    scala

---
+++ fibonacci.scala
@@ -0,0 +1,21 @@
+object MainFun{
+ 
+
+  def fibonacci(n:Int):Int={
+    if(n==0) {
+        return 0
+    }
+    else if(n==1) {
+        return 1
+    }
+    else {
+        return fibonacci(n-1) + fibonacci(n-2)
+    }
+}
+
+ def main(args:Array[String]) = { 
+ 
+   println("Fibonacci :" + fibonacci(10))
+ }
+
+}  
