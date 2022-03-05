f444e9cb82d507f1b4c5bedecfff2585db58b02b
ndrohith09 <ndrohith09@gmail.com>
2022-03-05 08:55:44 +0530

    scala

--- sets.scala
+++ sets.scala
@@ -8,6 +8,9 @@ def sets = {
         val hs1: HashSet[String] = hs ++ HashSet[String]("Java", "Scala")
         println(s"Add more than one HashSets = $hs1")
 
+        val movieSet = Set("Hitch", "Poltergeist", "Shrek")
+        println("movieSet=" + movieSet)
+
 }  
  def main(args:Array[String]) = {
 sets  
