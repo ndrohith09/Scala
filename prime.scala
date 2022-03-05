e30db6d155de451ff3da665794073066623e1b5e
ndrohith09 <ndrohith09@gmail.com>
2022-03-05 08:40:35 +0530

    scala

--- prime.scala
+++ prime.scala
@@ -1,3 +1,11 @@
 object Prime{
-    
+    def prime(n:Int):Boolean = {
+        if(n == 1) return false
+        if(n == 2) return true
+        if(n % 2 == 0) return false
+        for(i <- 3 to Math.sqrt(n).toInt by 2){
+            if(n % i == 0) return false
+        }
+        return true
+    }
 }
