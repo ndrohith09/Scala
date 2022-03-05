7eb71339cacd38340d4f7164b17a3cdf655ca891
ndrohith09 <ndrohith09@gmail.com>
2022-03-05 08:46:04 +0530

    scala

--- example.scala
+++ example.scala
@@ -4,6 +4,14 @@ object MainObject{
 def scalaExample = {  
     val s = new String("Hello Scala")
     println(s) 
+    val greetStrings = new Array[String](3)
+    greetStrings(0) = "Hello"
+    greetStrings(1) = ", "
+    greetStrings(2) = "world!\n"
+    for (i <- 0 to 2)
+      print(greetStrings(i))
 }  
-scalaExample   
+ def main(args:Array[String]) = {
+scalaExample  
+ } 
 }
